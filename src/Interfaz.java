ad848c0c3e33f1b65e81e79a0f7e15bd747a131f
njuante <juan.nunez1@alu.uclm.es>
2023-03-22 22:41:45 +0100

    trabajo programacion puerto entrega dos.

---
+++ src/Interfaz.java
@@ -0,0 +1,25 @@
+package Puerto;
+
+import javax.swing.*;
+
+public class Interfaz {
+    private JTextField TextoID;
+    private JTextField TextoPeso;
+    private JTextField TextoPais;
+    private JTextField TextoPrio;
+    private JButton BCrear;
+    private JButton BMostrar;
+    private JTextField TextoEmisor;
+    private JTextField TextoDesc;
+    private JTextField TextoReceptor;
+    private JCheckBox AduanaSi;
+    private JCheckBox AduanaNO;
+    private JLabel TIdentificador;
+    private JLabel TPeso;
+    private JLabel TPais;
+    private JLabel TPrioridad;
+    private JLabel TDescripcion;
+    private JLabel TNomEmisor;
+    private JLabel TNomReceptor;
+    private JLabel TAduana;
+}
---
+++ src/Puerto.java
@@ -0,0 +1,58 @@
+
+public class Puerto {
+    Contenedor[][] puerto = new Contenedor[10][12];
+
+    public Puerto(){}
+    public Puerto(Contenedor[][] puerto){
+        if(puerto!=null) this.puerto=puerto;
+    }
+
+    public String toString(){
+        String plano = new String();
+        for (int i = 0; i < puerto.length; i++) {
+            for (int j = 0; j < puerto[i].length; j++) {
+                if(puerto[i][j]!=null) plano+= "[O]";
+                else plano+="[L]";
+            }
+            plano+="\n";
+        }
+        return plano;
+    }
+
+    public void apilar(Contenedor contenedor){
+        if(contenedor!=null) {
+            if (contenedor.getPrio() == 1 || contenedor.getPrio() == 2) {
+                int colum=contenedor.getPrio()-1;
+                if (contenedor.getPrio() == 1) {
+                    for (int i = puerto.length-1; i > -1; --i) {
+                        if (puerto[i][colum] == null) {
+                            puerto[i][colum] = contenedor;
+                            return;
+                        }
+                    }
+                }
+            }
+            else{
+                for (int i = 2; i < puerto[0].length ; --i) {
+                    for (int j = puerto.length-1; j > -1; --j) {
+                        if(puerto[i][j]==null){
+                            puerto[i][j]=contenedor;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void desapilar(int colum){
+        if(colum<0 || colum>=puerto.length) return;
+        for (int i = 0; i < puerto.length; i++) {
+            if(puerto[i][colum]!=null){
+                Contenedor vacio = puerto[i][colum];
+                return;
+            }
+        }
+        return;
+    }
+}
